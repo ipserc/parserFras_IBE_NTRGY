352ae0af0e72c8407e4629532466ce423059e5e4
ipserc <ipserc@users.noreply.github.com>
2021-03-27 03:23:34 +0100

    __version__ = 2.2
    __version__ = 2.2
    __date__ = '2021-03-22'
    __updated__ = '2021-03-27'

--- parserFrasIberdrola.py
+++ parserFrasIberdrola.py
@@ -33,7 +33,7 @@
 		  -upw <string>        : user password (for encrypted files)
 		  -q                   : don't print any messages or errors
 		  -cfg <string>        : configuration file to use in place of .xpdfrc
-		  -v                   : print copyright and version info
+		  -v                   : print copyright and version __INFO__
 		  -h                   : print usage information
 		  -help                : print usage information
 		  --help               : print usage information
@@ -52,9 +52,9 @@
 """
 
 __all__ = []
-__version__ = 1.1
+__version__ = 2.2
 __date__ = '2021-03-22'
-__updated__ = '2021-03-23'
+__updated__ = '2021-03-27'
 
 import os
 import io
@@ -66,27 +66,35 @@ from pathlib import Path
 import pdftotext
 
 ##################################
-# CONSTANTES
+# PSEUDO-CONSTANTS
 ##################################
-DEBUG_ON = True
-INFO = "INFO"
-WARN = "WARN"
-ERROR = "ERROR"
-CMD_PDFTOTEXT = os.path.join(os.path.dirname(os.path.abspath(__file__)),"xpdf-tools-linux-4.02/bin64/pdftotext")
+__DEBUG_ON__ = False
+__INFO__ = "INFO"
+__WARN__ = "WARN"
+__ERROR__ = "ERROR"
+__PDFTOTEXT_PATH__ = "xpdf-tools-linux-4.02/bin64/pdftotext"
+__CMD_PDFTOTEXT__ = os.path.join(os.path.dirname(os.path.abspath(__file__)), __PDFTOTEXT_PATH__)
+__IBRDL__ = "IBRDL"
+__NTRGY__ = "NTRGY"
 
-
+##################################
+# BASE FUNCTIONS
+##################################
 def sprintf(*args, **kwargs):
     sio = io.StringIO()
     print(*args, **kwargs, end="", file=sio)
     return sio.getvalue()
     
 def printDebug(*args, **kwargs):
-	if DEBUG_ON:
+	if __DEBUG_ON__:
 		print("["+str(datetime.datetime.now())+"] - DEBUG :" + sprintf(*args, **kwargs))
 
 def printTraza(strTipoTRAZA, *args, **kwargs):
 	print("["+str(datetime.datetime.now())+"] - "+strTipoTRAZA+" :" + sprintf(*args, **kwargs))
 
+#################################
+# USELESS FUNCTIONS JUTS FOR TEST
+#################################
 def readAfile():
 	path = "/home/ipserc/Documentos/Facturas/Iberdrola.Electricidad Galileo 108/2021"
 	file = "2021-03-04-21.54.25.530080.pdf.txt"
@@ -102,10 +110,11 @@ def playWithDirs():
 	absDirName = os.path.dirname(os.path.abspath(__file__))
 	print("This script absolute dir name is ", absDirName)
 
+
 ##################################
 # ELECTRICIDAD
 ##################################
-def getMatchData_ELEC_IBE(matchStrToken, matchStrData, lineNbr, theText):
+def getMatchData_ELEC(matchStrToken, matchStrData, lineNbr, theText):
 	data = []
 	factMatch = re.match(matchStrToken, theText[lineNbr])
 	if factMatch:
@@ -114,13 +123,14 @@ def getMatchData_ELEC_IBE(matchStrToken, matchStrData, lineNbr, theText):
 		# ---printDebug("Next lineNbr: ", lineNbr)
 		# ---printDebug("Next line   : ", theText[lineNbr])
 		factMatch = re.match(matchStrData, theText[lineNbr])
-		matchGroups = factMatch.groups()
-		for item in matchGroups:
-			# ---printDebug("item:" + item)
-			data.append(item)
+		if factMatch:
+			matchGroups = factMatch.groups()
+			for item in matchGroups:
+				# ---printDebug("item:" + item)
+				data.append(item)
 	return data
 
-def extractInfo_ELEC_IBE(theText):
+def extractInfo_ELEC_IBRDL(theText):
 	sectFecha = ".*Fecha emisión factura: +(.*de +\d{4})"
 	sectPoten = "Peaje +acceso +potencia"
 	sectComer = "Comercialización"
@@ -129,88 +139,153 @@ def extractInfo_ELEC_IBE(theText):
 	sectCost = "Coste +energía"
 	dataLine2 = "(\d+\.\d+|\d+) +kWh +x +(\d,\d+) +\/kWh"
 	dataRow = {"fechaFactura":"","mesaño":"","potenciaContratada":"","diasFacturacion":"","terminoPotenciaDiario":"",\
-	"terminoCosteDiario":"","potenciaConsumida":"", "precioTermEnerPeajAcc":"","precioTermCostHorEner":""}
+	"terminoCosteDiario":"","energiaConsumida":"", "precioTermEnerPeajAcc":"","precioTermCostHorEner":""}
 
 	# ---printDebug("teText:", theText)
 	for lineNbr in range(0, len(theText)):
 		# Fecha emisión factura
 		fechaFactura = re.match(sectFecha, theText[lineNbr])
 		if fechaFactura:
-			printTraza(INFO, "Extrayendo Fecha emisión factura", "")	
+			printTraza(__INFO__, "Extrayendo Fecha emisión factura", "")	
 			fechaFactura = fechaFactura.group(1)
 			# mesAnio =  re.match("\d+ de (\w+) de (\d+)", fechaFactura)
 			# mes = mesAnio[1] 
 			# anio = mesAnio[2]
 			locale.setlocale(locale.LC_ALL, 'es_ES.utf8')
 			dtObj = datetime.datetime.strptime(fechaFactura, '%d de %B de %Y')
-			# ---printDebug("Fecha emisión factura:",fechaFactura)
-			# ---printDebug("Mes emisión factura  :",dtObj.month)
-			# ---printDebug("Año emisión factura  :",dtObj.year)
+			printDebug("Fecha emisión factura:",fechaFactura)
+			printDebug("Mes emisión factura  :",dtObj.month)
+			printDebug("Año emisión factura  :",dtObj.year)
 			dataRow.update({"fechaFactura":fechaFactura, "mesaño":sprintf(dtObj.month,"/",dtObj.year).replace(" ","")})
+			continue
 
 		# Peaje acceso potencia
-		data = getMatchData_ELEC_IBE(sectPoten, dataLine1, lineNbr, theText)
+		data = getMatchData_ELEC(sectPoten, dataLine1, lineNbr, theText)
 		if data:
-			printTraza(INFO, "Extrayendo Peaje acceso potencia", "")	
+			printTraza(__INFO__, "Extrayendo Peaje acceso potencia", "")	
 			potenciaContratada = data[0]
 			diasFacturacion = data[1]
 			terminoPotenciaDiario = data[2]
-			# ---printDebug("potenciaContratada   :" + potenciaContratada)
-			# ---printDebug("diasFacturacion      :" + diasFacturacion)
-			# ---printDebug("terminoPotenciaDiario:" + terminoPotenciaDiario)
-			#potenciaContratada = potenciaContratada.replace(" kW", "")
-			#diasFacturacion = diasFacturacion.replace(" días", "")
-			#terminoPotenciaDiario = terminoPotenciaDiario.replace(" /kW día", "")
+			printDebug("potenciaContratada   :" + potenciaContratada)
+			printDebug("diasFacturacion      :" + diasFacturacion)
+			printDebug("terminoPotenciaDiario:" + terminoPotenciaDiario)
 			dataRow.update({"potenciaContratada":potenciaContratada, "diasFacturacion":diasFacturacion, "terminoPotenciaDiario":terminoPotenciaDiario})
+			continue
 
 		# Comercialización
-		data = getMatchData_ELEC_IBE(sectComer, dataLine1, lineNbr, theText)
+		data = getMatchData_ELEC(sectComer, dataLine1, lineNbr, theText)
 		if data:
-			printTraza(INFO, "Extrayendo Comercialización", "")	
+			printTraza(__INFO__, "Extrayendo Comercialización", "")	
 			potenciaContratada = data[0]
 			diasFacturacion = data[1]
 			terminoCosteDiario = data[2]
-			# ---printDebug("potenciaContratada   :" + potenciaContratada)
-			# ---printDebug("diasFacturacion      :" + diasFacturacion)
-			# ---printDebug("terminoCosteDiario   :" + terminoCosteDiario)
-			#terminoCosteDiario = terminoCosteDiario.replace(" /kW día", "")
+			printDebug("potenciaContratada   :" + potenciaContratada)
+			printDebug("diasFacturacion      :" + diasFacturacion)
+			printDebug("terminoCosteDiario   :" + terminoCosteDiario)
 			dataRow.update({"terminoCosteDiario":terminoCosteDiario})
+			continue
 
 		# Peaje acceso energía
-		data = getMatchData_ELEC_IBE(sectEner, dataLine2, lineNbr, theText)
+		data = getMatchData_ELEC(sectEner, dataLine2, lineNbr, theText)
 		if data:
-			printTraza(INFO, "Extrayendo Peaje acceso energía", "")	
-			potenciaConsumida = data[0]
+			printTraza(__INFO__, "Extrayendo Peaje acceso energía", "")	
+			energiaConsumida = data[0]
 			precioTermEnerPeajAcc = data[1]
-			# ---printDebug("potenciaConsumida    :" + potenciaConsumida)
-			# ---printDebug("precioTermEnerPeajAcc:" + precioTermEnerPeajAcc)
-			#potenciaConsumida = potenciaConsumida.replace("kWh", "")
-			#precioTermEnerPeajAcc = precioTermEnerPeajAcc.replace(" /kWh", "")
-			dataRow.update({"potenciaConsumida":potenciaConsumida, "precioTermEnerPeajAcc":precioTermEnerPeajAcc})
+			printDebug("energiaConsumida    :" + energiaConsumida)
+			printDebug("precioTermEnerPeajAcc:" + precioTermEnerPeajAcc)
+			dataRow.update({"energiaConsumida":energiaConsumida, "precioTermEnerPeajAcc":precioTermEnerPeajAcc})
+			continue
 
 		# Coste energía
-		data = getMatchData_ELEC_IBE(sectCost, dataLine2, lineNbr, theText)
+		data = getMatchData_ELEC(sectCost, dataLine2, lineNbr, theText)
 		if data:
-			printTraza(INFO, "Extrayendo Coste energía", "")	
-			#potenciaConsumida = data[0]
+			printTraza(__INFO__, "Extrayendo Coste energía", "")	
+			#energiaConsumida = data[0]
 			precioTermCostHorEner = data[1]
-			# ---printDebug("potenciaConsumida    :" + potenciaConsumida)
-			# ---printDebug("precioTermCostHorEner:" + precioTermCostHorEner)
-			#precioTermCostHorEner = precioTermCostHorEner.replace(" /kWh", "")
+			printDebug("energiaConsumida    :" + energiaConsumida)
+			printDebug("precioTermCostHorEner:" + precioTermCostHorEner)
 			dataRow.update({"precioTermCostHorEner":precioTermCostHorEner})
 			
 	return dataRow
 
-def pdfToTxt_ELEC_IBE(filepath, excelDataTable):
-	pdfFP = filepath
-	txtFP = filepath+".txt"
-	os.system(CMD_PDFTOTEXT + " -table '%s' '%s'" % (pdfFP, txtFP))
-	fr = codecs.open(txtFP, "r", "ISO-8859-1")
-	printTraza(INFO, "Factura PDF convertida a txt:", txtFP)	
-	excelDataTable.append(extractInfo_ELEC_IBE(fr.readlines()))
-	# ---printDebug("excelDataTable:", excelDataTable)
-	
-def getDataFromPdf_ELEC_IBE(iniPath):
+def extractInfo_ELEC_NTRGY(theText):
+	sectFecha = ".*Fecha emisión factura: +(.*de +\d{4})"
+	sectPeaje = " +Importe +por +peaje +de +acceso"
+	sectComer = " +Importe +por +margen +de +comercialización +fijo"
+	dataLine1 = " +(\d+,\d+).* (\d+,\d+).*\((\d+)\/\d{3}"
+	sectCost = " +Importe +por +coste +de +la +energía"
+	dataLine2 = " +(\d+) +kWh .* (\d+,\d+) \/kWh"
+	dataRow = {"fechaFactura":"","mesaño":"","potenciaContratada":"","diasFacturacion":"","terminoPotenciaDiario":"",\
+	"terminoCosteDiario":"","energiaConsumida":"", "precioTermEnerPeajAcc":"","precioTermCostHorEner":""}
+
+	# ---printDebug("teText:", theText)
+	for lineNbr in range(0, len(theText)):
+		# Fecha emisión factura
+		fechaFactura = re.match(sectFecha, theText[lineNbr])
+		if fechaFactura:
+			printTraza(__INFO__, "Extrayendo Fecha emisión factura", "")	
+			fechaFactura = fechaFactura.group(1)
+			# mesAnio =  re.match("\d+ de (\w+) de (\d+)", fechaFactura)
+			# mes = mesAnio[1] 
+			# anio = mesAnio[2]
+			locale.setlocale(locale.LC_ALL, 'es_ES.utf8')
+			dtObj = datetime.datetime.strptime(fechaFactura, '%d de %B de %Y')
+			printDebug("Fecha emisión factura:",fechaFactura)
+			printDebug("Mes emisión factura  :",dtObj.month)
+			printDebug("Año emisión factura  :",dtObj.year)
+			dataRow.update({"fechaFactura":fechaFactura, "mesaño":sprintf(dtObj.month,"/",dtObj.year).replace(" ","")})
+			continue
+
+		# Peaje acceso potencia
+		data = getMatchData_ELEC(sectPeaje, dataLine1, lineNbr, theText)
+		if data:
+			printTraza(__INFO__, "Extrayendo Importe por peaje de acceso:", "POTENCIA")	
+			potenciaContratada = data[0]
+			terminoPotenciaDiario = data[1]
+			diasFacturacion = data[2]
+			printDebug("potenciaContratada   :" + potenciaContratada)
+			printDebug("diasFacturacion      :" + diasFacturacion)
+			printDebug("terminoPotenciaDiario:" + terminoPotenciaDiario)
+			dataRow.update({"potenciaContratada":potenciaContratada, "diasFacturacion":diasFacturacion, "terminoPotenciaDiario":terminoPotenciaDiario})
+			continue
+
+		# Comercialización
+		data = getMatchData_ELEC(sectComer, dataLine1, lineNbr, theText)
+		if data:
+			printTraza(__INFO__, "Extrayendo Importe por margen de comercialización fijo:", "POTENCIA")	
+			potenciaContratada = data[0]
+			terminoCosteDiario = data[1]
+			diasFacturacion = data[2]
+			printDebug("potenciaContratada   :" + potenciaContratada)
+			printDebug("diasFacturacion      :" + diasFacturacion)
+			printDebug("terminoCosteDiario   :" + terminoCosteDiario)
+			dataRow.update({"terminoCosteDiario":terminoCosteDiario})
+			continue
+			
+		# Peaje acceso energía
+		data = getMatchData_ELEC(sectPeaje, dataLine2, lineNbr, theText)
+		if data:
+			printTraza(__INFO__, "Extrayendo Importe por peaje de acceso:", "ENERGIA")	
+			energiaConsumida = data[0]
+			precioTermEnerPeajAcc = data[1]
+			printDebug("energiaConsumida    :" + energiaConsumida)
+			printDebug("precioTermEnerPeajAcc:" + precioTermEnerPeajAcc)
+			dataRow.update({"energiaConsumida":energiaConsumida, "precioTermEnerPeajAcc":precioTermEnerPeajAcc})
+			continue
+
+		# Coste energía
+		data = getMatchData_ELEC(sectCost, dataLine2, lineNbr, theText)
+		if data:
+			printTraza(__INFO__, "Extrayendo Coste:", "ENERGIA")	
+			#energiaConsumida = data[0]
+			precioTermCostHorEner = data[1]
+			printDebug("energiaConsumida    :" + energiaConsumida)
+			printDebug("precioTermCostHorEner:" + precioTermCostHorEner)
+			dataRow.update({"precioTermCostHorEner":precioTermCostHorEner})
+
+	return dataRow
+
+def getDataFromPdf_ELEC(comp, iniPath):
 	excelDataTable = []
 	# ---printDebug("iniPath:", iniPath)
 	for root, dirs, files in os.walk(iniPath):
@@ -220,17 +295,29 @@ def getDataFromPdf_ELEC_IBE(iniPath):
 			if re.match(".*\d{4}", root):
 				filepath = os.path.join(root, fileName)
 				if filepath.endswith(".pdf"):
-					printTraza(INFO, "Procesando Factura PDF:", filepath)
-					pdfToTxt_ELEC_IBE(filepath, excelDataTable)
+					printTraza(__INFO__, "Procesando Factura PDF:", filepath)
+					pdfToTxt_ELEC(comp, filepath, excelDataTable)
+						
 	# ---printDebug("excelDataTable:", excelDataTable)
 	return excelDataTable
 
-def writeToCSV_ELEC_IBE(iniPath, excelDataTable):
+def pdfToTxt_ELEC(comp, filepath, excelDataTable):
+	pdfFP = filepath
+	txtFP = filepath+".txt"
+	os.system(__CMD_PDFTOTEXT__ + " -table '%s' '%s'" % (pdfFP, txtFP))
+	fr = codecs.open(txtFP, "r", "ISO-8859-1")
+	printTraza(__INFO__, "Factura PDF convertida a txt:", txtFP)	
+	if comp == __IBRDL__:
+		excelDataTable.append(extractInfo_ELEC_IBRDL(fr.readlines()))
+	elif comp == __NTRGY__:
+		excelDataTable.append(extractInfo_ELEC_NTRGY(fr.readlines()))
+
+def writeToCSV_ELEC(comp, iniPath, excelDataTable):
 	rowStr = "fechaFactura"+";"+\
 	"mesanno"+";"+\
 	"potenciaContratada"+";"+\
 	"diasFacturacion"+";"+\
-	"potenciaConsumida"+";"+\
+	"energiaConsumida"+";"+\
 	"terminoPotenciaDiario"+";"+\
 	"terminoCosteDiario"+";"+\
 	"precioTermEnerPeajAcc"+";"+\
@@ -240,14 +327,14 @@ def writeToCSV_ELEC_IBE(iniPath, excelDataTable):
 		rowData["mesaño"]+";"+\
 		rowData["potenciaContratada"]+";"+\
 		rowData["diasFacturacion"]+";"+\
-		rowData["potenciaConsumida"]+";"+\
+		rowData["energiaConsumida"]+";"+\
 		rowData["terminoPotenciaDiario"]+";"+\
 		rowData["terminoCosteDiario"]+";"+\
 		rowData["precioTermEnerPeajAcc"]+";"+\
 		rowData["precioTermCostHorEner"]+os.linesep
 	# ---printDebug("rowStr:", rowStr)
-	csvFile = os.path.join(iniPath, "excelDataTable_ELEC_IBE.csv")
-	printTraza(INFO, "Guardando Data Table ELEC_IBE en:", csvFile) 
+	csvFile = os.path.join(iniPath, "excelDataTable_ELEC_"+comp+".csv")
+	printTraza(__INFO__, "Guardando Data Table ELEC_IBE en:", csvFile) 
 	with open(csvFile, "wt") as fw:
 		fw.write(rowStr)
 
@@ -271,7 +358,7 @@ def getDataFromPdf_GAS_NTRGY(iniPath):
 			if re.match(".*\d{4}", root):
 				filepath = os.path.join(root, fileName)
 				if filepath.endswith(".pdf"):
-					printTraza(INFO, "Procesando Factura PDF:", filepath)
+					printTraza(__INFO__, "Procesando Factura PDF:", filepath)
 					subExcelDT = pdfToTxt_GAS_NTRGY(filepath)
 					# ---printDebug("+ + + + + + subExcelDT:", subExcelDT)
 					for dtRow in subExcelDT:
@@ -283,10 +370,10 @@ def getDataFromPdf_GAS_NTRGY(iniPath):
 def pdfToTxt_GAS_NTRGY(filepath):
 	pdfFP = filepath
 	txtFP = filepath+".txt"
-	os.system(CMD_PDFTOTEXT + " -table '%s' '%s'" % (pdfFP, txtFP))
+	os.system(__CMD_PDFTOTEXT__ + " -table '%s' '%s'" % (pdfFP, txtFP))
 	# ---printDebug("Parsing ", txtFP,". . .")
 	fr = codecs.open(txtFP, "r", "ISO-8859-1")
-	printTraza(INFO, "Factura PDF convertida a txt:", txtFP)	
+	printTraza(__INFO__, "Factura PDF convertida a txt:", txtFP)	
 	subExcelDT = extractInfo_GAS_NTRGY(fr.readlines())
 	# ---printDebug("* * * * * * subExcelDT:", subExcelDT)
 	return (subExcelDT)
@@ -306,7 +393,7 @@ def extractInfo_GAS_NTRGY(theText):
 		# Fecha periodo factura
 		setcFechaFactura = re.match(sectFecha, theText[lineNbr])
 		if setcFechaFactura:
-			printTraza(INFO, "Extrayendo Fecha periodo factura", "")	
+			printTraza(__INFO__, "Extrayendo Fecha periodo factura", "")	
 			fixPartDataRow = {}
 			fechaFacturaIni = setcFechaFactura.group(1).replace(".", "/")
 			fechaFacturaFin = setcFechaFactura.group(2).replace(".", "/")
@@ -329,7 +416,7 @@ def extractInfo_GAS_NTRGY(theText):
 			sectTermi = 1			
 			# ---printDebug("Término fijo:", sectTermFijo)
 		if sectTermFijo:
-			printTraza(INFO, "Extrayendo Término Fijo(", sectTermi, ")")	
+			printTraza(__INFO__, "Extrayendo Término Fijo(", sectTermi, ")")	
 			fixTermDataRow = {}
 			numDias = sectTermFijo.group(1)
 			terminoFijo = sectTermFijo.group(2)
@@ -350,7 +437,7 @@ def extractInfo_GAS_NTRGY(theText):
 			# ---printDebug("Consumo gas:", matchConsumoGas)
 			consuGas = 1
 		if matchConsumoGas:
-			printTraza(INFO, "Extrayendo Consumo gas(", consuGas, ")")	
+			printTraza(__INFO__, "Extrayendo Consumo gas(", consuGas, ")")	
 			periodoDataRow = {}
 			if consuGas == 2:
 				periodoIni = matchConsumoGas.group(1).replace(".", "/")
@@ -405,7 +492,7 @@ def writeToCSV_GAS_NTRGY(iniPath, excelDataTable):
 		rowData["precioEnergia"]+os.linesep
 	# ---printDebug("rowStr:", rowStr)
 	csvFile = os.path.join(iniPath, "excelDataTable_GAS_NTRGY.csv")
-	printTraza(INFO, "Guardando Data Table GAS_NTRGY en:", csvFile) 
+	printTraza(__INFO__, "Guardando Data Table GAS_NTRGY en:", csvFile) 
 	with open(csvFile, "wt") as fw:
 		fw.write(rowStr)
 
@@ -421,12 +508,17 @@ def printStats():
 def main():
 	printStats()
 	iniPath = "/home/ipserc/Documentos/Facturas/Iberdrola.Electricidad Galileo 108"
-	printTraza(INFO, "Procesando Facturas de ELECTRICIDAD de ", "IBERDROLA")
-	excelDataTable = getDataFromPdf_ELEC_IBE(iniPath)
-	writeToCSV_ELEC_IBE(iniPath, excelDataTable)
+	printTraza(__INFO__, "Procesando Facturas de ELECTRICIDAD de ", "IBERDROLA")
+	excelDataTable = getDataFromPdf_ELEC(__IBRDL__, iniPath)
+	writeToCSV_ELEC(__IBRDL__, iniPath, excelDataTable)
+
+	iniPath = "/home/ipserc/Documentos/Facturas/Naturgy Electricidad Cangas"
+	printTraza(__INFO__, "Procesando Facturas de ELECTRICIDAD de ", "NATURGY")
+	excelDataTable = getDataFromPdf_ELEC(__NTRGY__, iniPath)
+	writeToCSV_ELEC(__NTRGY__, iniPath, excelDataTable)
 
 	iniPath = "/home/ipserc/Documentos/Facturas/Gas Natural Fenosa"
-	printTraza(INFO, "Procesando Facturas de GAS de ", "NATURGY")
+	printTraza(__INFO__, "Procesando Facturas de GAS de ", "NATURGY")
 	excelDataTable = getDataFromPdf_GAS_NTRGY(iniPath)
 	writeToCSV_GAS_NTRGY(iniPath, excelDataTable)
 
